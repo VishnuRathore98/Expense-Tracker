e8d132a30679ce7866ea7d971b98ca4cea3b4354
VishnuRathore98 <rathorevishnu666@gmail.com>
2024-07-01 05:56:59 +0530

    Created new project, added screens, and installed navigation packages.

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+function RecentExpenses() {
+    return(
+        <View><Text>RecentExpenses Screen</Text></View>
+    )
+}
+
+export default RecentExpenses;
