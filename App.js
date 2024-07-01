446346912e9891364cc9a5e0832df19c40c83b8d
VishnuRathore98 <rathorevishnu666@gmail.com>
2024-07-01 08:43:18 +0530

    Added basic stack and bottom tab navigations.

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
 
-export default function App() {
+import ManageExpense from "./screens/ManageExpense";
+import AllExpenses from "./screens/AllExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
+import { useState } from "react";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  const [isAllExpensesSelected, setAllExpensesSelected] = useState(true);
+  const [isRecentExpensesSelected, setRecentExpensesSelected] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Expense Tracker</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          tabBarIcon: () => {
+            return (
+              <Icon
+                name={isAllExpensesSelected ? "book" : "book-outline"}
+                size={24}
+              />
+            );
+          },
+        }}
+      />
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          tabBarIcon: () => {
+            return (
+              <Icon
+                name={
+                  isRecentExpensesSelected ? "clipboard" : "clipboard-outline"
+                }
+                size={24}
+              />
+            );
+          },
+        }}
+      />
+    </BottomTabs.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
